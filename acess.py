08b7a490df60b0ce588cbd42a2e4412bfb3ce853
WestFS <wf0403trabalho@gmail.com>
2024-06-06 23:58:15 -0300

    login de acesso -06/06/2024-

--- acess.py
+++ acess.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import Image
+
 
 
 def check_login(username, password):
@@ -12,15 +13,16 @@ def check_login(username, password):
     conn.close()
     return user
 
-def center_window(window, width, height):
+def center_window(window):
+    window.update_idletasks()
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    x = (screen_width // 2) - (width // 2)
-    y = (screen_height // 2) - (height // 2)
-    window.geometry(f'{width}x{height}+{x}+{y}')
-
+    x = (screen_width - window.winfo_width()) // 2
+    y = (screen_height - window.winfo_height()) // 2
+    window.geometry(f"+{x}+{y}")
 
 def login_screen():
+
     def attempt_login():
         username = username_entry.get()
         password = password_entry.get()
@@ -31,43 +33,70 @@ def login_screen():
             main_screen()
         else:
             messagebox.showerror("Erro", "Usuário ou senha incorretos.")
-
     def toggle_password_visibility():
         if password_entry.cget('show') == '*':
-            password_entry.config(show='')
-            toggle_button.config(image=eye_closed_image)
+            password_entry.configure(show='')
+            toggle_button.configure(image=eye_closed_ctk_image)
         else:
-            password_entry.config(show='*')
-            toggle_button.config(image=eye_open_image)
+            password_entry.configure(show='*')
+            toggle_button.configure(image= eye_open_ctk_image)
 
     login_window = ctk.CTk()
-    window_width, window_height = 300, 180
-    center_window(login_window, window_width, window_height)
+    login_window.geometry("720x450")
+    login_window.resizable(False, False)
     login_window.title("LOGIN")
 
-    login_frame = ctk.CTkFrame(login_window)
-    login_frame.pack(padx=10, pady=10)
+    eye_open_image = Image.open("eye.png")
+    eye_open_ctk_image = ctk.CTkImage(light_image=eye_open_image, size=(20, 20))
+    eye_closed_image = Image.open("eye-slash.png")
+    eye_closed_ctk_image = ctk.CTkImage(light_image=eye_closed_image, size=(20, 20))
 
-    username_label = ctk.CTkLabel(login_frame, text="username")
-    username_label.grid(row=0, column=0, padx=5, pady=5)
+    icon_user = Image.open("icon_login.png")
+    icon_user_ctk_image = ctk.CTkImage(light_image=icon_user, size=(125,125))
 
-    username_entry = ctk.CTkEntry(login_frame)
-    username_entry.grid(row=0, column=1, padx=5, pady=5)
+    frame_left = ctk.CTkFrame(login_window, width=360, height=450, fg_color="purple")
+    frame_left.grid(row=1, column=0, sticky="nsew")
 
-    password_label = ctk.CTkLabel(login_frame, text="password")
-    password_label.grid(row=1, column=0, padx=5, pady=5)
+    frame_right = ctk.CTkFrame(login_window, width=360, height=450, fg_color="transparent")
+    frame_right.grid(row=1, column=2, rowspan=2, sticky="nsew")
 
-    password_entry = ctk.CTkEntry(login_frame, show='*')
-    password_entry.grid(row=1, column=1, padx=5, pady=5)
+    icon_label = ctk.CTkLabel(frame_right, image=icon_user_ctk_image,text="")
+    icon_label.place(x=180, y=60, anchor="center")
 
-    toggle_button = ctk.CTkButton(login_frame, text="Mostrar Senha", command=toggle_password_visibility)
-    toggle_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+    username_label = ctk.CTkLabel(frame_right, text="Username")
+    username_label.place(x=180,y=135, anchor="center")
 
-    login_button = ctk.CTkButton(login_frame, text="Login", command=attempt_login)
-    login_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+    username_entry = ctk.CTkEntry(frame_right)
+    username_entry.place(x=180,y=160, anchor="center")
+
+    password_label = ctk.CTkLabel(frame_right, text="Password")
+    password_label.place(x=180,y=200, anchor="center")
+
+    password_entry = ctk.CTkEntry(frame_right, show='*')
+    password_entry.place(x=180, rely=0.5, anchor="center")
 
 
+    toggle_button = ctk.CTkButton(frame_right,text="", image=eye_open_ctk_image, command=toggle_password_visibility,
+                                  fg_color="transparent", width=2, height=2)
+    toggle_button.place(x=265, rely=0.5, anchor="center")
+
+    login_button = ctk.CTkButton(frame_right, text="Login", command=attempt_login, fg_color="purple")
+    login_button.place(x=180,rely=0.6, anchor="center")
+
+
+    center_window(login_window)
     login_window.mainloop()
+
+# Função para a tela principal após login bem-sucedido
+def main_screen():
+    main_window = ctk.CTk()
+    main_window.title("Tela Principal")
+    main_window.geometry("720x450")
+    main_window.resizable(False, False)
+
+    ctk.CTkLabel(main_window, text="Bem-vindo!").pack(pady=20)
+
+    main_window.mainloop()
 
 if __name__ == "__main__":
     login_screen()
